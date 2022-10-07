403cc38b71bdf8cf04dfd3fa186c3b0098596b87
ylfeng250 <1119030015@qq.com>
2022-10-07 22:40:42 +0800

    删除不需要的依赖

--- test/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import LV3Editor from '../src/index';
-import '../src/main.scss';
-
-it('renders', () => {
-  const wrapper = shallow(<LV3Editor />);
-  expect(wrapper.find('.LV3Editor').length).toBe(1);
-});
